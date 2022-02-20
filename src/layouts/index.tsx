f9b53db62db149b05b7ff5801e6f82f08474cc2d
qin-todo-team-j4 <sh071332@gmail.com>
2022-02-20 11:20:32 +0900

    refactor:ディレクトリの整理

---
+++ src/layouts/index.tsx
@@ -0,0 +1,9 @@
+import { ReactNode, VFC } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout: VFC<Props> = (props) => {
+  return <div>{props.children}</div>;
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Layout } from "../layouts";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Qin Todo</title>
+        <meta name="description" content="Qin Todo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+
+export default MyApp;
