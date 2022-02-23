2beef19925df5f1bd66b761c97b80e72babfa8ba
qin-todo-team-j4 <sh071332@gmail.com>
2022-02-23 23:00:57 +0900

    test:useTodoの初期値、state.isCreate更新テストを作成

---
+++ __tests__/todo/todo.spec.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { RenderResult } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
+import { describe, expect } from "vitest";
+
+import type { UseTodoReturnType } from "./../../src/hooks/useTodo";
+import { useTodo } from "./../../src/hooks/useTodo";
+
+let result: RenderResult<UseTodoReturnType>;
+
+beforeEach(() => {
+  result = renderHook(() => {
+    return useTodo();
+  }).result;
+});
+
+describe("useTodoのテスト", () => {
+  it("stateの初期値がvalue:'',isCreate:false,todoList:[]になっていること", () => {
+    expect(result.current.state.value).toBe("");
+    expect(result.current.state.isCreate).toBe(false);
+    expect(result.current.state.todoList.length).toBe(0);
+  });
+
+  it("addTask関数でisCreateがtrueになること", () => {
+    act(() => {
+      result.current.addTask();
+    });
+
+    expect(result.current.state.isCreate).toBeTruthy();
+  });
+});
