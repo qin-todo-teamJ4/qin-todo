b7e3c5645a1bb7041acb4caad9a6e62058ff842a
qin-todo-team-j4 <sh071332@gmail.com>
2022-02-23 18:02:16 +0900

    feat:ルートページでTodoListコンポーネントを読み込み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 
 import { Header } from "../components/Layouts/Header";
+import { TodoList } from "../components/todo/TodoList";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <div className="flex">
+        <TodoList title="今日する" mainColor="text-rose-500" />
+        <TodoList title="明日する" mainColor="text-orange-500" />
+        <TodoList title="今度する" mainColor="text-yellow-400" />
+      </div>
     </>
   );
 };
