b2a3e1fbcecba861689d78fae460c7b465b8821a
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-03 22:05:30 +0900

    feat: 仮アイコン削除＆アバター設置

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import type { VFC } from "react";
-import { AiOutlineUser } from "react-icons/ai";
+import { useUser } from "src/lib/auth";
+
+import { UserMenu } from "../user/UserMenu";
 
 export const Header: VFC = () => {
+  const user = useUser();
+
   return (
     <header className="h-20 flex justify-between">
       <div className="pt-6 xl:ml-48 md:ml-36 sm:ml-24 ml-12">
         <Image src="/logo.svg" width={112.48} height={24.14} alt="Qin Todo" />
       </div>
       <div className="pt-5 xl:mr-48 md:mr-36 sm:mr-24 mr-12">
-        <AiOutlineUser className="text-4xl  text-gray-300" />
+        <UserMenu user={user} />
       </div>
     </header>
   );
