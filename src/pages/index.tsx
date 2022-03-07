30373b082844b83341475c6b4748a82a8d4201a4
qin-todo-team-j4 <sh071332@gmail.com>
2022-03-07 22:49:22 +0900

    refactor:動作確認用のコードを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,6 @@ import { Header } from "../components/Layouts/Header";
 import { TodoList } from "../components/todo/TodoList";
 
 const Home: NextPage = () => {
-  const handle = async () => {
-    const res = await fetch("/users");
-    const todoRes = await fetch("/todos");
-    const users = await res.json();
-    const todos = await todoRes.json();
-    // eslint-disable-next-line no-console
-    console.log(users);
-    // eslint-disable-next-line no-console
-    console.log(todos);
-  };
   const router = useRouter();
   const user = useUser();
   if (user === null) router.push("/auth/signin");
@@ -27,7 +17,6 @@ const Home: NextPage = () => {
         <TodoList title="今日する" mainColor="text-rose-500" />
         <TodoList title="明日する" mainColor="text-orange-500" />
         <TodoList title="今度する" mainColor="text-yellow-400" />
-        <button onClick={handle}>ボタン</button>
       </div>
     </>
   );
